a611d69f9c1c650c6db6db2505f5c17cdb6d4593
Harsh-Kushwaha-75 <harshhk7500@gmail.com>
2025-05-13 02:16:28 +0530

    Linking

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -35,8 +35,8 @@ const Footer = () => {
                     </a>
                   </li>
                   <li>
-                    <a href="wa.me/919259797411" target="_blank" rel='noopener noreferrer'>
-                      <img src={assets.whatsapp} className='w-4 h-5 mr-2'  alt="" />
+                    <a href="https://wa.me/919259797411" target="_blank" rel='noopener noreferrer'>
+                      <img src={assets.whatsapp} className='w-5 h-5 mr-2'  alt="" />
                     </a>
                   </li>
                 </ul>
